a5098f507c91d91bc59ae4ecfccf776f3579422a
EstebanPechuan <esteban.pechuan@gmail.com>
2022-05-24 23:32:08 -0300

    NavBar

--- susten-clean-shop/src/Navbar/Navbar.js
+++ susten-clean-shop/src/Navbar/Navbar.js
@@ -1,5 +1,41 @@
-import { Navbar } from "./Navbar.css";
+import "./Navbar.css";
+import logo from '../img/logo.JPG';
 
-function Navbar() {
-    
+export function Navbar() {
+    return (
+        <header>
+            <div className="Logo">
+                <a href="#">
+                    <img src={logo} />
+                </a>
+            </div>
+            
+            <nav className="Nav">
+                <ul className="Menu">
+                    <li className="NavItem">
+                        <a href="#">Home</a>
+                    </li>
+                    <li className="NavItem DropMenu">
+                        <a href="#">Productos</a>
+                        <ul className="SubMenu">
+                            <li className="NavItem">
+                                <a href="#">Productos para cocinas industriales</a>
+                            </li>
+                            <li className="NavItem">
+                                <a href="#">Productos para hospitales</a>
+                            </li>
+                            <li className="NavItem">
+                                <a href="#">Limpieza de pisos</a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+
+                <div className="Login">
+                    <button className="BtnGhost">Login</button>
+                    <button className="BtnFill">Registrarse</button>
+                </div>
+            </nav>
+        </header>
+      );
 }
